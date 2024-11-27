a922047b30ed67af2cc21cf1da97d22c0949e398
Agda Silva <agdasilvap@gmail.com>
2024-11-27 02:14:29 -0300

    fix: ajustes nas permissões de acesso locais

--- api/v1/api.py
+++ api/v1/api.py
@@ -5,6 +5,5 @@ from api.v1.endpoints import address
 
 api_router = APIRouter()
 
-
 api_router.include_router(address.router, prefix='/addresses', tags=['addresses'])
 api_router.include_router(user.router, prefix='/users', tags=['users'])
