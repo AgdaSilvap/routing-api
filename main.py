6ba987545bc40eab33313f7087835221bbd471ce
Agda Silva <agdasilvap@gmail.com>
2024-11-27 02:15:45 -0300

    fix: ajustes na autenticação de usuários

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
 from core.configs import settings
 from api.v1.api import api_router
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI(title="Trabalho de EMPR")
+
+# Configuração do CORS
+origins = [
+    "http://localhost",  # Permite o acesso de localhost
+    "http://localhost:5500", # Se seu frontend estiver rodando em localhost:3000 (React, Vue, etc.)
+    "http://127.0.0.1:5500",  # Caso use o 127.0.0.1 no lugar de localhost
+]
+
+# Adiciona o middleware CORS ao FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Define quais domínios podem fazer requisição
+    allow_credentials=True,
+    allow_methods=["*"],  # Permite todos os métodos HTTP (GET, POST, PUT, DELETE, etc.)
+    allow_headers=["*"],  # Permite todos os cabeçalhos
+)
+
 app.include_router(api_router, prefix=settings.API_V1_STR)
 
 
